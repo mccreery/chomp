fcbc7d25ca3d92a77885ca4ac774453d2c6a9f16
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-10-23 04:15:34 +0100

    Add character animation

---
+++ compass.gd
@@ -0,0 +1,15 @@
+enum {LEFT, RIGHT, UP, DOWN}
+
+static func get_direction(angle):
+    angle = fposmod(angle, PI * 2)
+
+    if angle < PI / 4:
+        return RIGHT
+    elif angle < PI * 3 / 4:
+        return UP
+    elif angle < PI * 5 / 4:
+        return LEFT
+    elif angle < PI * 7 / 4:
+        return DOWN
+    else:
+        return RIGHT
--- main_character.gd
+++ main_character.gd
@@ -1,6 +1,21 @@
 extends KinematicBody2D
+const Compass = preload("compass.gd")
 
 export (int) var speed = 500
+
+export (String) var left = "left"
+export (String) var right = "right"
+export (String) var up = "up"
+export (String) var down = "down"
+
+onready var animations = {
+    Compass.LEFT: left,
+    Compass.RIGHT: right,
+    Compass.UP: up,
+    Compass.DOWN: down
+}
+
+onready var sprite = $AnimatedSprite
 
 func get_input_direction():
     return Vector2(
@@ -8,6 +23,14 @@ func get_input_direction():
         Input.get_action_strength("down") - Input.get_action_strength("up")
     ).clamped(1)
 
+func update_sprite(velocity):
+    if velocity != Vector2(0, 0):
+        var direction = Compass.get_direction(atan2(-velocity.y, velocity.x))
+        sprite.play(animations[direction])
+    else:
+        sprite.stop()
+
 func _physics_process(_delta):
     var velocity = get_input_direction() * speed
     velocity = move_and_slide(velocity)
+    update_sprite(velocity)
