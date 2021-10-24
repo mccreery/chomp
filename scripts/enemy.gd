e82608e479fa7b6b5ced823caa3afea06e70d5e4
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-10-24 14:25:43 +0100

    Add eat mechanic and how to play isntructions

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,5 @@
 extends Character
+class_name Enemy
 
 const EPS = 2
 
--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,8 +1,12 @@
 extends Character
 
 var max_health = 4
+var health = 1 setget set_health
 
-var health = 4 setget set_health
+func play_sfx(stream):
+    if !$AudioStreamPlayer2D.is_playing():
+        $AudioStreamPlayer2D.stream = stream
+        $AudioStreamPlayer2D.play()
 
 func set_health(value):
     health = clamp(value, 0, max_health)
@@ -10,6 +14,7 @@ func set_health(value):
         die()
 
 var game_over_sound = preload("res://sfx/game_over.wav")
+var eat_sound = preload("res://sfx/chomp.wav")
 
 func get_input_direction():
     return Vector2(
@@ -20,11 +25,24 @@ func get_input_direction():
 func _process(_delta):
     walk_direction = get_input_direction()
 
+    if Input.is_action_just_pressed("interact") and close_enemy != null:
+        close_enemy.queue_free()
+        play_sfx(eat_sound)
+        self.health += 1
+
 func die():
     set_process(false)
-    if !$AudioStreamPlayer2D.is_playing():
-        $AudioStreamPlayer2D.stream = game_over_sound
-        $AudioStreamPlayer2D.play()
+    play_sfx(game_over_sound)
 
 func _on_AudioStreamPlayer2D_finished():
-    get_tree().call_deferred("change_scene", "res://scenes/menu.tscn")
+    if $AudioStreamPlayer2D.stream == game_over_sound:
+        get_tree().call_deferred("change_scene", "res://scenes/menu.tscn")
+
+var close_enemy = null
+func _on_range_trigger_body_entered(body):
+    if body is Enemy && close_enemy == null:
+        close_enemy = body
+
+func _on_range_trigger_body_exited(body):
+    if body == close_enemy:
+        close_enemy = null
